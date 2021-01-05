a52f7ba02da2cfe3f2d0ff06a61b96b37b90f6cb
Hersonmei <herson.me@hotmail.com>
2021-01-04 22:52:22 -0300

    Operador booletano &&

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// var a = true;
+// var b = true;
+
+
+// var c = a && b;
+
+
+// console.log(c)
+
+
+var idade = 19;
+
+var maior20 = idade > 20;
+var menor30 = idade < 30;
+
+var entre = maior20 && menor30;
+
+console.log("Idade", idade)
+console.log("Maior que 20", maior20)
+console.log("Maior que 30", menor30)
+console.log("Entre", entre)
