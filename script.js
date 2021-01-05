5bd41168ae7b84b19a1e68e378141ed6289c2313
Hersonmei <herson.me@hotmail.com>
2021-01-04 22:54:02 -0300

    operador booleano || ou

--- script.js
+++ script.js
@@ -9,14 +9,14 @@
 // console.log(c)
 
 
-var idade = 19;
+var idade = 67;
 
-var maior20 = idade > 20;
-var menor30 = idade < 30;
+var menor10 = idade <= 10;
+var maior65 = idade >= 65;
 
-var entre = maior20 && menor30;
+var gratuidade = menor10 || maior65;
 
 console.log("Idade", idade)
-console.log("Maior que 20", maior20)
-console.log("Maior que 30", menor30)
-console.log("Entre", entre)
+console.log("Menor que 10", menor10)
+console.log("Maior que 65", maior65)
+console.log("Tem direito a gratuidade", gratuidade)
