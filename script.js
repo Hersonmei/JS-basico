07881b3f7bd1dc290b93d210c6cb8f38fec3413d
Hersonmei <herson.me@hotmail.com>
2021-01-05 21:45:43 -0300

    Considicionais if, else, else if

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 
+var idade = 44;
 
-var str = "Valor \"texto entre aspas\" qualquer";
+if (idade >= 35){
+    console.log("Pode!")
+    console.log("Qual o seu pedido?")
+}
 
-console.log(str)
-
-
-// Adição se tratando de string, vai ser uma concatenação.
-var string = "Valor qualquer";
-string += " outro texto";
-
-console.log(string)
-
-
-
-// Sempre que aparecer no console azul, vai ser um número. É possível fazer essa soma de uma string com um número, no entando vai ser uma concatenação na verdade, não uma soma em si. Tanto que o número 8 deixa de ser azul e fica preto.
-var numero = 8;
-var string = "Valor qualquer";
-string += " outro texto " + numero;
-
-console.log(numero)
-console.log(string)
+else if (idade >= 18){
+    console.log("pode")
+    console.log("Mostre a identidade!")
+}
+else {
+    console.log("não pode")
+    console.log("volte futuramente")
+}
