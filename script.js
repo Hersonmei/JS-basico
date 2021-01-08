2de8f0469bc6dd349bc858b8070a572c0df9c5fa
Hersonmei <herson.me@hotmail.com>
2021-01-08 12:47:36 -0300

    while

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
 var numero = 5;
 
-for(var i = 0; i < numero; i++){
+while(numero < 10){
 
-    console.log("Executando o for, pela " + i + " vez");
-
-
-}
-
- console.log("acabou");
-
-
- var numero1 = 5;
-
-for(var j = 3; j < numero; j++){
-
-    console.log("Executando o for, pela " + j + " vez");
-
+    console.log("Número " + numero);
+    numero++;
 
 }
 
  console.log("acabou");
 
 
-
- var numero2 = 5;
-
-for(var k = 10; k < numero; k++){
-
-    console.log("Executando o for, pela " + k + " vez");
-
-
-}
-
- console.log("acabou");
-
-//  Nesse exemplo, colocamos a variável k para repetir desse número até menor que a variável número. Quando o resultado for verdadeiro ela vai mostrar a mensagem do console log até o valor, depois quando ficar negativa, vai mostrar o valor do console fora da chave.
-
-// No primeiro exemplo normal, no segundo ele começa a contagem a partir do 3, mostrando só dois resultados, e no terceiro exemplo, como o k é 10, maior que 5, não vai mostrar nada e vai direto para o acabou.
+//  while também é uma forma de loop ou laço, sempre ter o cuidado para não gerar um loop infinito, gerando um bug.
