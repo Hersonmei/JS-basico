d404c271f96783e47b43b9be309eff339223ca9d
Hersonmei <herson.me@hotmail.com>
2021-01-12 12:10:07 -0300

    Juntando tudo até aqui

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 
-function media(n1, n2) {
-var nota1 = n1;
-var nota2 = n2;
-var media = (nota1 + nota2) / 2;
+//Nome do aluno - nota 1 - nota 2 - média - Aprovado/Reprovado
+
+var nomes = ["Igor", "José", "Maria"]
+var notasA = [7.0, 6.5, 9.5]
+var notasB = [8.0, 7.0, 8.5]
 
 
-return media
+function media(n1, n2){
+        return (n1 + n2) / 2;
+}
+
+
+function passou(media){
+
+    if (media > 7) {
+        return "Aprovado";
+    } 
+    else {
+        return "Reprovado"
+    }
 
 }
 
-var resultado = media(6, 7);
-var m = media;
 
 
-var resultado2 = m(2, 3)
+for (var index in nomes) {
 
-console.log(resultado);
-console.log(resultado2);
+    var nota1 = notasA[index];  //criou-se essas variáveis, para tirar ela da fóruma abaixo, deixando mais simples.
+    var nota2 = notasB[index];
 
-// Pornto, no primeiro caso, pelo que pude entender, vais er igual o da aula anterior, que a função eu posso executá-la e atribuir um resultado a ela, ou seja, retornar um resultado a ela. 
+    var m =  media(nota1, nota2)
 
-//Ou eu posso nessa aula, que seria o resultado 2, atribuir a função a uma variável, no caso eu peguei a função (média) e atribuí a variável (m), depois sim eu peguei a variável m e dei os valores. Então, prova que dá certo, atribuir a função a uma variável.
+    console.log(nomes[index] + " - " + nota1 + " - " + nota2 +  " - " + m + " - " + passou(m));
 
-//Partindo desse pressuposto, no exemplo abaixo vamos atribuir a função diretamente a uma variável, nesse caso como já estamos fazendo isso diretamente, (NÃO PRECISAMOS DAR NOME A FUNÇÃO), logo ficaria assim:
-
-var media = function (n1, n2){
-
-    return (n1 + n2) / 2
 }
-
-var m = media (5, 6)
-
-console.log(m);
-
-// Ou podemos tbm declaralar o console.lo de uma maneira mais direta, sem necessidade de criar a variável (m).
-
-console.log(media (7, 8));
