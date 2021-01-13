adc929b9e591d7703b49375a09d8ee586893db47
Hersonmei <herson.me@hotmail.com>
2021-01-13 12:17:37 -0300

    Objeto - Inserindo funções dentro e evitando a repetição de diversas funções.

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-
+//Quando um objeto tem uma variável, temos nesse caso uma propriedade, mas quando temos uma função, vamos ter um método!
 
 var aluno = {
             nome: "Herson", 
-            notas : [7.5, 8.0] 
+            notas : [7, 8],
             
+            media: function(n1, n2){
+                return (n1 + n2) / 2;
+            }
 }            
 
-aluno.matricula = 12345;
+
+var aluno1 = {
+    nome: "Joao", 
+    notas : [5, 8],
+    
+    media: function(n1, n2){
+        return (n1 + n2) / 2;
+    }
+}   
+
+console.log(aluno.nome); 
+console.log(aluno.media(aluno.notas[0], aluno.notas[1])); 
+
+console.log(aluno1.nome); 
+console.log(aluno1.media(aluno1.notas[0], aluno1.notas[1])); 
 
 
-aluno["sobrenome"] = "Meireles";
-
-
-var novaProp = "nome do meio"; //Essa e de baixo são só 1 forma a mais de inserir propriedade.
-
-aluno[novaProp] = "Alexandre";
+// A primeira modificação que podemos fazer é em relação a fução, caso eu queria alterar e não queria fazer a mudança em todos, por exemplo se for uma furma grande, eu posso tirar a função de dentro do objetivo, dar um nome a mesma, de media a média dentro do objeto vai chamar essa função. Ex. abaixo:
 
 
 
-console.log(aluno); 
+function calcMedia(n1, n2){   
+    return (n1 + n2) / 2; 
 
-//Esses (3) exemplos acima, são duas formas de inserir propriedades no meu objetivo mesmo após ele criado.
+} 
 
+var aluno2 = {
+    nome: "Vitor", 
+    notas : [7, 8],
+    
+    media: calcMedia
+}
+           
+
+
+var aluno3 = {
+nome: "Amanda", 
+notas : [5, 8],
+
+media: calcMedia
+}   
+
+console.log(aluno2.nome); 
+console.log(aluno2.media(aluno2.notas[0], aluno2.notas[1])); 
+
+console.log(aluno3.nome); 
+console.log(aluno3.media(aluno3.notas[0], aluno3.notas[1])); 
