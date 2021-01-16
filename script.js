6a11f19b4f33c54dcaca4e9358c19b0aba575888
Hersonmei <herson.me@hotmail.com>
2021-01-16 11:42:18 -0300

    Data - Só algumas, mas existem várias funções que podem ser utulizadas nas datas.

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-// Aula de resumão de objetos, mostrando as três diferentes formas de construir um.
 
-
-var a = {
-    nome: "Igor",
-    sobrenome: "Oliveira"
-}
-
-console.log(a);
-console.log(a.nome);
-console.log(a.sobrenome);
+var d = new Date("05-05-1990");
 
 
 
-
-
-
-//Essa segunda forma é o objeto dentro de uma função. Reparas nas formas que podemos chamar no console.log.
-
-function bbj(n, l){
-    return {
-        name: n,
-        lastname: l
-    }
-}
-
-var english = bbj("Herson", "Meireles")
-
-
-console.log(english);
-console.log(english.name);
-console.log(english["lastname"]);
-
-
-
-
-
-
-//A terceira forma é uma mais elaborada, que gera as INSTÂNCIAS, que tbm precisa utilizar o (new).
-
-function abc(e, m){
-
-    this.nome1 = e;
-    this.sobrenome2 = m;
-}
-
-var c = new abc("Ivson", "Antonio")
-
-console.log(c);
-console.log(c.nome1);
-console.log(c["sobrenome2"]);
+console.log(d);
+console.log(d.getDay());
+console.log(d.getDate());
+console.log(d.getMonth ());
+console.log(d.getFullYear());
